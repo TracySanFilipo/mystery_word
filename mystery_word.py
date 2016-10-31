4fa52c7de33499ded0749ba2fbda46c554eeadc8
TracySanFilipo <tracysanfilipo@gmail.com>
2016-10-30 22:56:29 -0400

    Final edits

--- mystery_word.py
+++ mystery_word.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-def try_to_get_difficulty_from_user():
+def difficulty_from_user():
     while True:
         difficulty = input("Choose a difficulty level: select 1 for easy, 2 for normal, or 3 for hard: ")
         if difficulty == '1':
@@ -13,7 +13,7 @@ def try_to_get_difficulty_from_user():
         else:
             print("Select 1 for easy, 2 for normal, or 3 for hard. No other selections can begin the game.")
 
-def obtain_words_of_appropriate_difficulty(difficulty_level_chosen):
+def obtain_wordlist(difficulty_level_chosen):
     with open("/usr/share/dict/words", "r") as f:
         dictionarywords = f.read()
         cleanwords = dictionarywords.split("\n")
@@ -43,7 +43,7 @@ def get_random_word(word_list):
 def reveal_length_of_word(wordlength):
     print("The mystery word is {} characters long.".format(wordlength))
 
-def display_blanks_and_guessed_letters(length, secret_word, good_guesses):
+def display_blanks_and_guessed_letters(secret_word, good_guesses):
     flashy_display_list = []
     for letters in secret_word:
         if letters in good_guesses:
@@ -67,30 +67,29 @@ def obtain_user_guess(bad_guesses, good_guesses):
             continue
 
 def check_for_complete_word(secret_word, good_guesses):
-    final_word = []
     for letters in secret_word:
         if letters in good_guesses:
-            final_word.append(letters)
+            continue
         else:
             return False
 
 def game():
-    difficulty_level = try_to_get_difficulty_from_user()
-    possibilities = obtain_words_of_appropriate_difficulty(difficulty_level)
+    difficulty_level = difficulty_from_user()
+    possibilities = obtain_wordlist(difficulty_level)
     the_mystery_word = get_random_word(possibilities)
-    obtain_length_of_word = len(the_mystery_word)
-    reveal_length_of_word(obtain_length_of_word)
+    word_length = len(the_mystery_word)
+    reveal_length_of_word(word_length)
     bad_guesses_number = 0
     bad_guesses = []
     good_guesses = []
-    display_blanks_and_guessed_letters(obtain_length_of_word, the_mystery_word, good_guesses)
+    display_blanks_and_guessed_letters(the_mystery_word, good_guesses)
     while bad_guesses_number < 8:
         guess = obtain_user_guess(bad_guesses, good_guesses)
         if len(guess) == 1:
             if guess in the_mystery_word:
                 good_guesses.append(guess)
                 print("Good job! The letter {} is in the mystery word! You still have {} guesses left".format(guess, (8 - bad_guesses_number)))
-                display_blanks_and_guessed_letters(obtain_length_of_word, the_mystery_word, good_guesses)
+                display_blanks_and_guessed_letters(the_mystery_word, good_guesses)
                 result = check_for_complete_word(the_mystery_word, good_guesses)
                 if result != False:
                     print("You win! The mystery word was {}".format(the_mystery_word))
@@ -99,7 +98,7 @@ def game():
                 bad_guesses_number += 1
                 bad_guesses.append(guess)
                 print("Sorry, {} is not in the word. You only have {} guesses remaining.".format(guess, (8 - bad_guesses_number)))
-                display_blanks_and_guessed_letters(obtain_length_of_word, the_mystery_word, good_guesses)
+                display_blanks_and_guessed_letters(the_mystery_word, good_guesses)
         else:
             continue
     if bad_guesses_number >= 8:
